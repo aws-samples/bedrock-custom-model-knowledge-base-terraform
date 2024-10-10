7410513889b7a9c738658512b4e75dc2e01f7f91
Manu Chandrasekhar <manuchn@amazon.com>
2024-10-10 17:39:51 -0400

    fix: variables, docs

--- pipeline/modules/repo/variables.tf
+++ pipeline/modules/repo/variables.tf
@@ -4,11 +4,6 @@
 #http://aws.amazon.com/agreement or other written agreement between Customer and either
 #Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 
-variable "source_repository_name" {
-  type        = string
-  description = "Name of the Source CodeCommit repository used by the pipeline"
-}
-
 variable "provider_type" {
   type        = string
   description = "Name of the external provider you are connecting to"
